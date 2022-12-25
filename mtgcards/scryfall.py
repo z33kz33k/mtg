58215c245eb3549df50f7f9b5f101bc0864bfc23
z33k <piotr.mazik@protonmail.com>
2022-12-25 22:01:35 +0100

    Add type line parsing in scryfall.py

--- mtgcards/scryfall.py
+++ mtgcards/scryfall.py
@@ -10,8 +10,9 @@
 import json
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 from functools import lru_cache
-from typing import Callable, Dict, Iterable, List, Optional, Set
+from typing import Callable, Dict, Iterable, List, Optional, Set, Tuple
 
 import scrython
 
@@ -41,19 +42,185 @@ MULTIPART_LAYOUTS = ['adventure', 'art_series', 'double_faced_token', 'flip', 'm
                      'transform']
 
 
+class Color(Enum):
+    WHITE = "W"
+    BLUE = "U"
+    BLACK = "B"
+    RED = "R"
+    GREEN = "G"
+    # allied pairs
+    AZORIUS = ("W", "U")
+    DIMIR = ("U", "B")
+    RAKDOS = ("B", "R")
+    GRUUL = ("R", "G")
+    SELESNYA = ("G", "W")
+    # enemy paiRs
+    ORZHOV = ("W", "B")
+    IZZET = ("U", "R")
+    GOLGARI = ("B", "G")
+    BOROS = ("R", "W")
+    SIMIC = ("G", "U")
+    # shard triples
+    BANT = ("G", "W", "U")
+    ESPER = ("W", "U", "B")
+    GRIXIS = ("U", "B", "R")
+    JUND = ("B", "R", "G")
+    NAYA = ("R", "G", "W")
+    # wedge triples
+    ABZAN = ("W", "B", "G")
+    JESKAI = ("U", "R", "W")
+    SULTAI = ("B", "G", "U")
+    MARDU = ("R", "W", "B")
+    TEMUR = ("G", "U", "R")
+    # quadruples
+    ARTIFICE = ("W", "U", "B", "R")
+    CHAOS = ("U", "B", "R", "B")
+    AGGRESSION = ("B", "R", "G", "W")
+    ALTRUISM = ("R", "G", "W", "U")
+    GROWTH = ("G", "W", "U", "B")
+    # other
+    ALL = ("W", "U", "B", "R", "G")
+    COLORLESS = "L"
+
+
 class TypeLine:
     """Parser of type line in Scryfall data.
     """
     SEPARATOR = "—"
 
+    # according to MtG Wiki
+    SUPERTYPES = {"Basic", "Elite", "Host", "Legendary", "Ongoing", "Snow", "Token", "World"}
+    PERMAMENT_TYPES = {"Artifact", "Creature", "Enchantment", "Land", "Planeswalker"}
+    NONPERMAMENT_TYPES = {"Sorcery", "Instant"}
+
+    # creature types
+    # race types
+    ICONIC_RACES = {
+        Color.WHITE: "Angel",
+        Color.BLUE: "Sphinx",
+        Color.BLACK: "Demon",
+        Color.RED: "Dragon",
+        Color.GREEN: "Hydra",
+    }
+    CHARACTERISTIC_RACES = {
+        Color.WHITE: {"Human"},
+        Color.BLUE: {"Merfolk"},
+        Color.BLACK: {"Vampire", "Zombie"},
+        Color.RED: {"Goblin"},
+        Color.GREEN: {"Elf"},
+    }
+    MECHANICALLY_THEMED_RACES = {"Atog", "Bringer", "Demigod", "God", "Incarnation", "Licid",
+                                 "Lhurgoyf", "Nephilim", "Phelddagrif", "Shapeshifter", "Slith",
+                                 "Sliver", "Spike", "Volver", "Wall", "Weird", "Werewolf", "Zubera"}
+    TOKEN_SPECIFIC_RACES = {"Balloon", "Camarid", "Caribou", "Fractal", "Germ", "Graveborn",
+                            "Hamster", "Inkling", "Mite", "Orb", "Pentavite", "Pincher", "Prism",
+                            "Sand", "Saproling", "Sculpture", "Servo", "Splinter", "Tetravite",
+                            "Triskelavite"}
+    MAJORITY_RACES = {
+        Color.WHITE: {"Archon", "Camel", "Cat", "Fox", "Griffin", "Hippogriff", "Kirin",
+                      "Kithkin", "Kor", "Lammasu", "Mouse", "Pegasus", "Soltari", "Unicorn"},
+        Color.BLUE: {"Beeble", "Cephalid", "Crab", "Djinn", "Drake", "Faerie", "Fish", "Homarid",
+                     "Homunculus", "Illusion", "Jellyfish", "Kraken", "Leviathan", "Metathran",
+                     "Moonfolk", "Nautilus", "Octopus", "Otter", "Oyster", "Serpent", "Shark",
+                     "Siren", "Sponge", "Squid", "Starfish", "Thalakos", "Trilobite", "Turtle",
+                     "Vedalken", "Whale"},
+        Color.BLACK: {"Aetherborn", "Azra", "Bat", "Carrier", "Dauthi", "Eye",
+                      "Gorgon", "Hag", "Harpy", "Horror", "Imp", "Lamia", "Leech",  "Nightmare",
+                      "Nightstalker", "Phyrexian", "Rat", "Scorpion", "Shade", "Skeleton", "Slug",
+                      "Specter", "Thrull", "Worm", "Wraith"},
+        Color.RED: {"Cyclops", "Devil", "Dwarf", "Efreet", "Giant", "Goat", "Gremlin", "Hellion",
+                    "Jackal", "Kobold", "Manticore", "Minotaur", "Ogre", "Orc", "Orgg", "Phoenix",
+                    "Viashino", "Wolverine", "Yeti"},
+        Color.GREEN: {"Antelope", "Ape", "Aurochs", "Badger", "Basilisk", "Bear", "Beast", "Boar",
+                      "Brushwagg", "Centaur", "Crocodile", "Dryad", "Elk", "Ferret", "Fungus",
+                      "Hippo", "Hyena", "Mole", "Mongoose", "Monkey", "Ooze", "Ouphe", "Plant",
+                      "Rabbit", "Raccoon", "Rhino", "Snake", "Spider", "Squirrel", "Treefolk",
+                      "Troll", "Wolf", "Wombat", "Wurm"},
+        Color.COLORLESS: {"Eldrazi"},
+    }
+    ARTIFACT_RACES = {"Assembly-Worker", "Blinkmoth", "Construct", "Dreadnought", "Juggernaut",
+                      "Gnome", "Golem", "Masticore", "Myr", "Robot", "Sable", "Scarecrow",
+                      "Thopter", "Walrus"}
+    MULTICOLORED_RACES = {"Alien", "Avatar", "Bird", "Chimera", "Mutant", "Naga", "Cockatrice",
+                          "Dinosaur", "Dog", "Elemental", "Elephant", "Sheep", "Frog",
+                          "Gargoyle", "Horse", "Insect", "Nymph", "Ox", "Pangolin", "Pest",
+                          "Kavu", "Lizard", "Satyr", "Noggle", "Reflection", "Salamander",
+                          "Spirit", "Surrakar"}
+    CROSSOVER_RACES = {
+        "Dungeons & Dragons": {"Beholder", "Gith", "Gnoll", "Halfling", "Tiefling"},
+        "Universes Beyond": {"Astartes", "C'tan", "Custodes", "Necron", "Primarch", "Tyranid"},
+    }
+    RACES = {*ICONIC_RACES.values(), *{race for v in CHARACTERISTIC_RACES.values() for race in v},
+             *MECHANICALLY_THEMED_RACES, *TOKEN_SPECIFIC_RACES,
+             *{race for v in MAJORITY_RACES.values() for race in v}, *ARTIFACT_RACES,
+             *MULTICOLORED_RACES, *{race for v in CROSSOVER_RACES.values() for race in v}}
+
+    # class types
+    SPELLCASTERS = {
+        Color.WHITE: "Cleric",
+        Color.BLUE: "Wizard",
+        Color.BLACK: "Warlock",
+        Color.RED: "Shaman",
+        Color.GREEN: "Druid",
+    }
+    MECHANICALLY_THEMED_CLASSES = {"Ally", "Coward", "Egg", "Flagbearer", "Mercenary", "Monger",
+                                   "Ninja", "Pilot", "Processor", "Rebel", "Samurai", "Spellshaper"}
+    TOKEN_SPECIFIC_CLASSES = {"Army", "Deserter", "Scion", "Serf", "Survivor", "Tentacle"}
+    GENERAL_CLASSES = {"Advisor", "Archer", "Artificer", "Assassin", "Barbarian", "Bard",
+                       "Berserker", "Child", "Citizen", "Clown", "Drone", "Elder", "Employee",
+                       "Gamer", "Guest", "Knight", "Minion", "Monk", "Mystic", "Noble", "Nomad",
+                       "Peasant", "Performer", "Pirate", "Praetor", "Ranger", "Rigger", "Rogue",
+                       "Scout", "Soldier", "Spawn", "Warrior"}
+    CROSSOVER_CLASSES = {"Universes Beyond": "Inquisitor"}
+    CLASSES = {*SPELLCASTERS.values(), *MECHANICALLY_THEMED_CLASSES, *TOKEN_SPECIFIC_CLASSES,
+               *GENERAL_CLASSES, *CROSSOVER_CLASSES.values()}
+
     @property
     def text(self) -> str:
         return self._text
 
-    def __init__(self, text: str) -> None:
-        self._text = text
+    @property
+    def supertypes(self) -> List[str]:
+        return [t for t in self._types if t in self.SUPERTYPES]
 
-    # TODO
+    @property
+    def regular_types(self) -> List[str]:
+        return [t for t in self._types if t not in self.SUPERTYPES]
+
+    @property
+    def subtypes(self) -> List[str]:
+        return self._subtypes
+
+    @property
+    def is_permanent(self) -> bool:
+        return all(p in self.PERMAMENT_TYPES for p in self.regular_types)
+
+    @property
+    def is_nonpermanent(self) -> bool:
+        # type not being permanent doesn't mean it's 'non-permanent', e.g. 'dungeon' is neither
+        return all(p in self.NONPERMAMENT_TYPES for p in self.regular_types)
+
+    @property
+    def races(self) -> List[str]:
+        return [t for t in self.subtypes if t in self.RACES]
+
+    @property
+    def classes(self) -> List[str]:
+        return [t for t in self.subtypes if t in self.CLASSES]
+
+    def __init__(self, text: str) -> None:
+        if MULTIPART_SEPARATOR in text:
+            raise ValueError("Multipart type line")
+        self._text = text
+        self._types, self._subtypes = self._parse()
+
+    def _parse(self) -> Tuple[List[str], List[str]]:
+        """Parse text into types and subtypes.
+        """
+        if self.SEPARATOR in self.text:
+            types, subtypes = self.text.split(f" {self.SEPARATOR} ", maxsplit=1)
+            return types.split(), subtypes.split()
+        return self.text.split(), []
 
 
 @dataclass(frozen=True)
@@ -69,6 +236,10 @@ class CardFace:
         return self.json["name"]
 
     @property
+    def name_parts(self) -> Set[str]:
+        return {*self.name.split()}
+
+    @property
     def mana_cost(self) -> str:
         return self.json["mana_cost"]
 
@@ -82,7 +253,32 @@ class CardFace:
 
     @property
     def colors(self) -> List[str]:
-        return self.json["colors"]
+        result = self.json.get("colors")
+        return result if result else []
+
+    @lru_cache
+    def parse_types(self) -> TypeLine:
+        return TypeLine(self.type_line)
+
+    @property
+    def supertypes(self) -> List[str]:
+        return self.parse_types().supertypes
+
+    @property
+    def regular_types(self) -> List[str]:
+        return self.parse_types().regular_types
+
+    @property
+    def subtypes(self) -> List[str]:
+        return self.parse_types().subtypes
+
+    @property
+    def races(self) -> List[str]:
+        return self.parse_types().races
+
+    @property
+    def classes(self) -> List[str]:
+        return self.parse_types().classes
 
 
 @dataclass(frozen=True)
@@ -122,8 +318,8 @@ class Card:
 
     @property
     def colors(self) -> List[str]:
-        colors = self.json.get("colors")
-        return colors if colors else []
+        result = self.json.get("colors")
+        return result if result else []
 
     @property
     def formats(self) -> List[str]:
@@ -175,6 +371,12 @@ class Card:
     @property
     def name(self) -> str:
         return self.json["name"]
+
+    @property
+    def name_parts(self) -> Set[str]:
+        if not self.is_multipart:
+            return {*self.name.split()}
+        return {part for face in self.card_faces for part in face.name_parts}
 
     @property
     def power(self) -> Optional[int]:
@@ -245,10 +447,33 @@ class Card:
 
         if self.legalities[fmt] == "legal":
             return True
-        elif self.legalities[fmt] == "not_legal":
-            return False
-        else:
-            raise ScryfallError(f"Unexpected value for {fmt!r} legality: {self.legalities[fmt]!r}")
+        return False
+
+    def is_banned_in(self, fmt: str) -> bool:
+        """Returns `True` if this card is banned in format designated by `fmt`.
+
+        :param fmt: Scryfall format designation
+        :raises: ValueError on invalid format designation
+        """
+        if fmt.lower() not in self.formats:
+            raise ValueError(f"No such format: {fmt!r}")
+
+        if self.legalities[fmt] == "banned":
+            return True
+        return False
+
+    def is_restricted_in(self, fmt: str) -> bool:
+        """Returns `True` if this card is restricted in format designated by `fmt`.
+
+        :param fmt: Scryfall format designation
+        :raises: ValueError on invalid format designation
+        """
+        if fmt.lower() not in self.formats:
+            raise ValueError(f"No such format: {fmt!r}")
+
+        if self.legalities[fmt] == "restricted":
+            return True
+        return False
 
     @staticmethod
     def _int(text: Optional[str]) -> Optional[int]:
@@ -259,6 +484,12 @@ class Card:
         except ValueError:
             return None
         return value
+
+    @lru_cache()
+    def parse_types(self) -> Optional[TypeLine]:
+        if self.is_multipart:
+            return None
+        return TypeLine(self.type_line)
 
 
 @lru_cache
@@ -275,12 +506,6 @@ def bulk_data() -> Set[Card]:
     return {Card(card_data) for card_data in data}
 
 
-def arena_data() -> Set[Card]:
-    """Return Scryfall bulk data filtered for only cards available on Arena.
-    """
-    return {card for card in bulk_data() if "arena" in card.games}
-
-
 def games(data: Optional[Iterable[Card]] = None) -> List[str]:
     """Return list of string designations for games that can be played with cards in Scryfall data.
     """
@@ -292,7 +517,18 @@ def games(data: Optional[Iterable[Card]] = None) -> List[str]:
     return sorted(result)
 
 
-def set_codes(data: Optional[Iterable[Card]] = None) -> List[str]:
+def colors(data: Optional[Iterable[Card]] = None) -> List[str]:
+    """Return list of string designations for MtG colors in Scryfall data.
+    """
+    data = data if data else bulk_data()
+    result = set()
+    for card in data:
+        result.update(card.colors)
+
+    return sorted(result)
+
+
+def sets(data: Optional[Iterable[Card]] = None) -> List[str]:
     """Return list of string codes for MtG sets in Scryfall data (e.g. 'bro' for The Brothers'
     War).
     """
@@ -327,20 +563,30 @@ def rarities(data: Optional[Iterable[Card]] = None) -> List[str]:
     return sorted({card.rarity for card in data})
 
 
-def set_cards(set_code: str) -> Set[Card]:
-    """Return card data for set designated by ``set_code``.
-
-    Returns an empty set if set code is invalid.
-    """
-    return {card for card in bulk_data() if card.set == set_code.lower()}
-
-
 def find_cards(predicate: Callable[[Card], bool],
                data: Optional[Iterable[Card]] = None) -> Set[Card]:
     """Return list of cards from ``data`` that satisfy ``predicate``.
     """
     data = data if data else bulk_data()
     return {card for card in data if predicate(card)}
+
+
+def set_cards(*set_codes: str, data: Optional[Iterable[Card]] = None) -> Set[Card]:
+    """Return card data for sets designated by ``set_codes``.
+    """
+    return find_cards(lambda c: c.set in [code.lower() for code in set_codes], data)
+
+
+def arena_cards() -> Set[Card]:
+    """Return Scryfall bulk data filtered for only cards available on Arena.
+    """
+    return find_cards(lambda c: "arena" in c.games)
+
+
+def format_cards(fmt: str, data: Optional[Iterable[Card]] = None) -> Set[Card]:
+    """Return card data for MtG format designated by ``fmt``.
+    """
+    return find_cards(lambda c: c.is_legal_in(fmt), data)
 
 
 def find_card(predicate: Callable[[Card], bool],
