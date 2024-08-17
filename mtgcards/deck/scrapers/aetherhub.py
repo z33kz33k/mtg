z33kz33k/mtg

commit cf933f16cc71cc781f88b1f9e8ca7343a9d0833f

Move deck scraper modules to a separate package. Rename `decks` package to `deck`. Other moving around and renaming

mtgcards/deck/scrapers/aetherhub.py
@@ -1,7 +1,7 @@
 """
 
-    mtgcards.decks.aetherhub.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mtgcards.deck.scrapers.aetherhub.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Parse AetherHub decklist page.
 
     @author: z33k
@@ -13,7 +13,8 @@ from datetime import datetime
 from bs4 import Tag
 
 from mtgcards.const import Json
-from mtgcards.decks import Archetype, Deck, DeckScraper, InvalidDeck, Mode
+from mtgcards.deck import Archetype, Deck, InvalidDeck, Mode
+from mtgcards.deck.scrapers import DeckScraper
 from mtgcards.scryfall import Card, COMMANDER_FORMATS
 from mtgcards.utils import extract_float, extract_int
 from mtgcards.utils.scrape import ScrapingError, getsoup
@@ -107,7 +108,7 @@ class AetherhubScraper(DeckScraper):
             self._metadata["meta"]["share_change"] = extract_float(change_part)
 
             count_tag = self._soup.select("h4.text-center.pt-2")[0]
-            count_text, _ = count_tag.text.strip().split("decks,")
+            count_text, _ = count_tag.text.strip().split("deck,")
             self._metadata["meta"]["count"] = extract_int(count_text)
 
     @classmethod

mtgcards/deck/scrapers/goldfish.py
@@ -1,7 +1,7 @@
 """
 
-    mtgcards.decks.goldfish.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mtgcards.deck.scrapers.goldfish.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Parse MtGGoldfish decklists.
 
     @author: z33k
@@ -13,7 +13,8 @@ from datetime import datetime
 from bs4 import Tag
 
 from mtgcards.const import Json
-from mtgcards.decks import Deck, DeckScraper, InvalidDeck, Mode, ParsingState
+from mtgcards.deck import Deck, InvalidDeck, Mode, ParsingState
+from mtgcards.deck.scrapers import DeckScraper
 from mtgcards.scryfall import Card, all_formats
 from mtgcards.utils import ParsingError, extract_int, timed
 from mtgcards.utils.scrape import ScrapingError, getsoup, http_requests_counted, throttled_soup
@@ -164,8 +165,8 @@ class GoldfishScraper(DeckScraper):
             return None
 
 
-@http_requests_counted("scraping meta decks")
-@timed("scraping meta decks", precision=1)
+@http_requests_counted("scraping meta deck")
+@timed("scraping meta deck", precision=1)
 def scrape_meta(fmt="standard") -> list[Deck]:
     fmt = fmt.lower()
     if fmt not in all_formats():

mtgcards/deck/scrapers/moxfield.py
@@ -1,7 +1,7 @@
 """
 
-    mtgcards.decks.moxfield.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mtgcards.deck.scrapers.moxfield.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Parse Moxfield decklist page.
 
     @author: z33k
@@ -11,7 +11,8 @@ import logging
 from datetime import datetime
 
 from mtgcards.const import Json
-from mtgcards.decks import Deck, DeckScraper, InvalidDeck
+from mtgcards.deck import Deck, InvalidDeck
+from mtgcards.deck.scrapers import DeckScraper
 from mtgcards.scryfall import Card
 from mtgcards.utils.scrape import timed_request
 
@@ -55,7 +56,7 @@ class MoxfieldScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "www.moxfield.com/decks/" in url
+        return "www.moxfield.com/deck/" in url
 
     def _scrape_metadata(self) -> None:  # override
         fmt = self._json_data["format"]

mtgcards/deck/scrapers/mtgazone.py
@@ -1,7 +1,7 @@
 """
 
-    mtgcards.decks.mtgazone.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mtgcards.deck.scrapers.mtgazone.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Parse MTG Arena Zone decklist page.
 
     @author: z33k
@@ -13,7 +13,8 @@ from datetime import datetime
 from bs4 import Tag
 
 from mtgcards.const import Json
-from mtgcards.decks import Deck, DeckScraper, InvalidDeck, Mode
+from mtgcards.deck import Deck, InvalidDeck, Mode
+from mtgcards.deck.scrapers import DeckScraper
 from mtgcards.scryfall import Card, ARENA_FORMATS
 from mtgcards.utils import extract_int, from_iterable, timed
 from mtgcards.utils.scrape import ScrapingError, getsoup
@@ -41,7 +42,7 @@ class MtgaZoneScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "mtgazone.com/user-decks/" in url or "mtgazone.com/deck/" in url
+        return "mtgazone.com/user-deck/" in url or "mtgazone.com/deck/" in url
 
     def _scrape_metadata(self) -> None:  # override
         name_author_tag = self._soup.find("div", class_="name-container")
@@ -146,7 +147,7 @@ def _parse_meta_deck(deck_tag: Tag, decks2tiers: dict[str, int], deck_place: int
     return deck
 
 
-@timed("scraping meta decks")
+@timed("scraping meta deck")
 def scrape_meta(fmt="standard", bo3=True) -> list[Deck]:
     formats = {fmt for fmt in ARENA_FORMATS if fmt not in {"brawl", "standardbrawl"}}
     formats = sorted({*formats, "pioneer"})

mtgcards/deck/scrapers/tappedout.py
@@ -1,7 +1,7 @@
 """
 
-    mtgcards.decks.tappedout.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mtgcards.deck.scrapers.tappedout.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Parse TappedOut decklist page.
 
     @author: z33k
@@ -10,8 +10,9 @@
 import logging
 
 from mtgcards.const import Json
-from mtgcards.decks import Deck, DeckScraper, InvalidDeck
-from mtgcards.decks.arena import ArenaParser
+from mtgcards.deck import Deck, InvalidDeck
+from mtgcards.deck.scrapers import DeckScraper
+from mtgcards.deck.arena import ArenaParser
 from mtgcards.utils import get_ago_date, extract_int
 from mtgcards.utils.scrape import getsoup
 
@@ -30,7 +31,7 @@ class TappedoutScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "tappedout.net/mtg-decks/" in url
+        return "tappedout.net/mtg-deck/" in url
 
     def _scrape_metadata(self) -> None:  # override
         fmt_tag = self._soup.select_one("a.btn.btn-success.btn-xs")

mtgcards/deck/scrapers/tcgplayer.py
@@ -1,7 +1,7 @@
 """
 
-    mtgcards.decks.tcgplayer.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mtgcards.deck.scrapers.tcgplayer.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Parse TCG Player decklist page.
 
     @author: z33k
@@ -14,7 +14,8 @@ import dateutil.parser
 from bs4 import Tag
 
 from mtgcards.const import Json
-from mtgcards.decks import Deck, DeckScraper, InvalidDeck
+from mtgcards.deck import Deck, InvalidDeck
+from mtgcards.deck.scrapers import DeckScraper
 from mtgcards.scryfall import Card
 from mtgcards.utils import extract_int
 from mtgcards.utils.scrape import ScrapingError, get_dynamic_soup_by_xpath, getsoup
@@ -33,7 +34,7 @@ class OldPageTcgPlayerScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "decks.tcgplayer.com/" in url
+        return "deck.tcgplayer.com/" in url
 
     def _scrape_metadata(self) -> None:  # override
         info_tag = self._soup.find("div", class_="viewDeckHeader")
@@ -79,6 +80,10 @@ class OldPageTcgPlayerScraper(DeckScraper):
             return None
 
 
+# TODO: there's actually a request to API for JSON I forgot to check:
+#  https://infinite-api.tcgplayer.com/deck/magic/{DECK_ID}
+#  /?source=infinite-content&subDecks=true&cards=true&stats=true - so this could probably be scraped
+#  bypassing the soup (and Selenium)
 class NewPageTcgPlayerScraper(DeckScraper):
     """Scraper of TCG Player new-style decklist page.
     """

mtgcards/deck/scrapers/untapped.py
@@ -1,7 +1,7 @@
 """
 
-    mtgcards.decks.untapped.py
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    mtgcards.deck.scrapers.untapped.py
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     Parse Untapped.gg decklist page.
 
     @author: z33k
@@ -13,8 +13,9 @@ from datetime import datetime
 import selenium.common.exceptions
 
 from mtgcards.const import Json
-from mtgcards.decks import Deck, DeckScraper
-from mtgcards.decks.arena import ArenaParser
+from mtgcards.deck import Deck
+from mtgcards.deck.scrapers import DeckScraper
+from mtgcards.deck.arena import ArenaParser
 from mtgcards.utils import extract_float, extract_int
 from mtgcards.utils.scrape import get_dynamic_soup_by_xpath
 
@@ -71,7 +72,7 @@ class UntappedRegularDeckScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "mtga.untapped.gg/decks/" in url
+        return "mtga.untapped.gg/deck/" in url
 
     @staticmethod
     def _normalize_url(url: str) -> str:
@@ -89,7 +90,7 @@ class UntappedRegularDeckScraper(DeckScraper):
 
 
 class UntappedMetaDeckScraper(DeckScraper):
-    """Scraper of Untapped meta-deck page.
+    """Scraper of Untapped meta-decks page.
     """
     def __init__(self, url: str, metadata: Json | None = None) -> None:
         super().__init__(url, metadata)
@@ -104,7 +105,7 @@ class UntappedMetaDeckScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "mtga.untapped.gg/meta/decks/" in url
+        return "mtga.untapped.gg/meta/deck/" in url
 
     def _scrape_metadata(self) -> None:  # override
         name_tag = self._soup.select_one("h1[class*='layouts__MetaPageHeaderTitle']")

mtgcards/yt.py
@@ -23,18 +23,18 @@ from contexttimer import Timer
 from youtubesearchpython import Channel as YtspChannel
 
 from mtgcards.const import Json
-from mtgcards.decks import Deck
-from mtgcards.decks.aetherhub import AetherhubScraper
-from mtgcards.decks.arena import ArenaParser, get_arena_lines
-from mtgcards.decks.cardhoarder import CardhoarderScraper
-from mtgcards.decks.goldfish import GoldfishScraper
-from mtgcards.decks.moxfield import MoxfieldScraper
-from mtgcards.decks.mtgazone import MtgaZoneScraper
-from mtgcards.decks.mtgtop8 import MtgTop8Scraper
-from mtgcards.decks.streamdecker import StreamdeckerScraper
-from mtgcards.decks.tappedout import TappedoutScraper
-from mtgcards.decks.tcgplayer import NewPageTcgPlayerScraper, OldPageTcgPlayerScraper
-from mtgcards.decks.untapped import UntappedProfileDeckScraper, UntappedRegularDeckScraper
+from mtgcards.deck import Deck
+from mtgcards.deck.scrapers.aetherhub import AetherhubScraper
+from mtgcards.deck.arena import ArenaParser, get_arena_lines
+from mtgcards.deck.scrapers.cardhoarder import CardhoarderScraper
+from mtgcards.deck.scrapers.goldfish import GoldfishScraper
+from mtgcards.deck.scrapers.moxfield import MoxfieldScraper
+from mtgcards.deck.scrapers.mtgazone import MtgaZoneScraper
+from mtgcards.deck.scrapers.mtgtop8 import MtgTop8Scraper
+from mtgcards.deck.scrapers.streamdecker import StreamdeckerScraper
+from mtgcards.deck.scrapers.tappedout import TappedoutScraper
+from mtgcards.deck.scrapers.tcgplayer import NewPageTcgPlayerScraper, OldPageTcgPlayerScraper
+from mtgcards.deck.scrapers.untapped import UntappedProfileDeckScraper, UntappedRegularDeckScraper
 from mtgcards.scryfall import all_formats
 from mtgcards.utils import extract_float, getrepr, timed
 from mtgcards.utils.gsheets import extend_gsheet_rows_with_cols, retrieve_from_gsheets_cols

commit 697c6939972bfd8cbeed4c55ab9c58cd402e34cb

Specify halting xpaths when scraping with Selenium (closes #27) Fix a bug in `multiply_by_symbol()` Revert some assheaded changes introduced with cf933f1 ("decks" ==> "deck" (thank you, Pycharm!))

mtgcards/deck/scrapers/aetherhub.py
@@ -107,7 +107,7 @@ class AetherhubScraper(DeckScraper):
             self._metadata["meta"]["share_change"] = extract_float(change_part)
 
             count_tag = self._soup.select("h4.text-center.pt-2")[0]
-            count_text, _ = count_tag.text.strip().split("deck,")
+            count_text, _ = count_tag.text.strip().split("decks,")
             self._metadata["meta"]["count"] = extract_int(count_text)
 
     @classmethod

mtgcards/deck/scrapers/goldfish.py
@@ -164,8 +164,8 @@ class GoldfishScraper(DeckScraper):
             return None
 
 
-@http_requests_counted("scraping meta deck")
-@timed("scraping meta deck", precision=1)
+@http_requests_counted("scraping meta decks")
+@timed("scraping meta decks", precision=1)
 def scrape_meta(fmt="standard") -> list[Deck]:
     fmt = fmt.lower()
     if fmt not in all_formats():

mtgcards/deck/scrapers/moxfield.py
@@ -56,7 +56,7 @@ class MoxfieldScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "www.moxfield.com/deck/" in url
+        return "www.moxfield.com/decks/" in url
 
     def _scrape_metadata(self) -> None:  # override
         fmt = self._json_data["format"]

mtgcards/deck/scrapers/mtgazone.py
@@ -42,7 +42,7 @@ class MtgaZoneScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "mtgazone.com/user-deck/" in url or "mtgazone.com/deck/" in url
+        return "mtgazone.com/user-decks/" in url or "mtgazone.com/deck/" in url
 
     def _scrape_metadata(self) -> None:  # override
         name_author_tag = self._soup.find("div", class_="name-container")
@@ -146,7 +146,7 @@ def _parse_meta_deck(deck_tag: Tag, decks2tiers: dict[str, int], deck_place: int
     return deck
 
 
-@timed("scraping meta deck")
+@timed("scraping meta decks")
 def scrape_meta(fmt="standard", bo3=True) -> list[Deck]:
     formats = {fmt for fmt in ARENA_FORMATS if fmt not in {"brawl", "standardbrawl"}}
     formats = sorted({*formats, "pioneer"})

mtgcards/deck/scrapers/tappedout.py
@@ -31,7 +31,7 @@ class TappedoutScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "tappedout.net/mtg-deck/" in url
+        return "tappedout.net/mtg-decks/" in url
 
     def _scrape_metadata(self) -> None:  # override
         fmt_tag = self._soup.select_one("a.btn.btn-success.btn-xs")

mtgcards/deck/scrapers/tcgplayer.py
@@ -34,7 +34,7 @@ class OldPageTcgPlayerScraper(DeckScraper):
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "deck.tcgplayer.com/" in url
+        return "decks.tcgplayer.com/" in url
 
     def _scrape_metadata(self) -> None:  # override
         info_tag = self._soup.find("div", class_="viewDeckHeader")

mtgcards/deck/scrapers/untapped.py
@@ -10,7 +10,7 @@
 import logging
 from datetime import datetime
 
-import selenium.common.exceptions
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
 from mtgcards.const import Json
 from mtgcards.deck import Deck
@@ -27,15 +27,22 @@ CLIPBOARD_XPATH = "//span[text()='Copy to MTGA']"
 class UntappedProfileDeckScraper(DeckScraper):
     """Scraper of decklist page of Untapped.gg user's profile.
     """
+    _NO_GAMES_XPATH = ("//div[text()='No games have been played with this deck in the selected "
+                       "time frame']")
+    _PRIVATE_XPATH = "//div[text()='This profile is private']"
+
     def __init__(self, url: str, metadata: Json | None = None) -> None:
         super().__init__(url, metadata)
         try:
             self._soup, _, self._clipboard = get_dynamic_soup_by_xpath(
-                self._url, CLIPBOARD_XPATH, consent_xpath=CONSENT_XPATH,
+                self._url, CLIPBOARD_XPATH, self._NO_GAMES_XPATH, self._PRIVATE_XPATH,
+                consent_xpath=CONSENT_XPATH,
                 clipboard_xpath=CLIPBOARD_XPATH)
             self._scrape_metadata()
             self._deck = self._get_deck()
-        except selenium.common.exceptions.TimeoutException:
+        except NoSuchElementException:
+            _log.warning("Scraping failed due to absence of the looked for element")
+        except TimeoutException:
             _log.warning(f"Scraping failed due to Selenium timing out")
 
     @staticmethod
@@ -66,12 +73,12 @@ class UntappedRegularDeckScraper(DeckScraper):
                 clipboard_xpath=CLIPBOARD_XPATH)
             self._scrape_metadata()
             self._deck = self._get_deck()
-        except selenium.common.exceptions.TimeoutException:
+        except TimeoutException:
             _log.warning(f"Scraping failed due to Selenium timing out")
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "mtga.untapped.gg/deck/" in url
+        return "mtga.untapped.gg/decks/" in url
 
     @staticmethod
     def _normalize_url(url: str) -> str:
@@ -99,12 +106,12 @@ class UntappedMetaDeckScraper(DeckScraper):
                 clipboard_xpath=CLIPBOARD_XPATH)
             self._scrape_metadata()
             self._deck = self._get_deck()
-        except selenium.common.exceptions.TimeoutException:
+        except TimeoutException:
             _log.warning(f"Scraping failed due to Selenium timing out")
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
-        return "mtga.untapped.gg/meta/deck/" in url
+        return "mtga.untapped.gg/meta/decks/" in url
 
     def _scrape_metadata(self) -> None:  # override
         name_tag = self._soup.select_one("h1[class*='layouts__MetaPageHeaderTitle']")

mtgcards/utils/__init__.py
@@ -261,13 +261,13 @@ def deserialize_dates(dct: dict) -> dict:
 def multiply_by_symbol(number: float, symbol: str) -> int:
     """Multiply ``number`` by ``symbol`` and return it.
     """
-    if symbol in 'Kk':
+    if symbol in ('K', 'k'):
         return int(number * 1_000)
-    elif symbol in 'Mm':
+    elif symbol in ('M', 'm'):
         return int(number * 1_000_000)
-    elif symbol in 'Bb':
+    elif symbol in ('B', 'b'):
         return int(number * 1_000_000_000)
-    elif symbol in 'Tt':
+    elif symbol in ('T', 't'):
         return int(number * 1_000_000_000_000)
     _log.warning(f"Unsupported symbol for multiplication: {symbol!r}")
     return int(number)

mtgcards/utils/scrape.py
@@ -21,9 +21,10 @@ import requests
 from bs4 import BeautifulSoup
 from requests.exceptions import HTTPError
 from selenium import webdriver
-from selenium.common import TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
@@ -194,7 +195,7 @@ def extract_source(url: str) -> str:
 
 @timed("getting dynamic soup")
 def get_dynamic_soup_by_xpath(
-        url: str, xpath: str, click=False, consent_xpath="", clipboard_xpath="",
+        url: str, xpath: str, *halt_xpaths, click=False, consent_xpath="", clipboard_xpath="",
         timeout=SELENIUM_TIMEOUT) -> tuple[BeautifulSoup, BeautifulSoup | None, str | None]:
     """Return BeautifulSoup object(s) from dynamically rendered page source at ``url`` using
     Selenium WebDriver that waits for presence of an element specified by ``xpath``.
@@ -209,6 +210,7 @@ def get_dynamic_soup_by_xpath(
     Args:
         url: webpage's URL
         xpath: XPath to locate the main element
+        halt_xpaths: XPaths to locate elements that should halt the wait
         click: if True, main element is clicked before returning the soups
         consent_xpath: XPath to locate a consent button (if present)
         clipboard_xpath: Xpath to locate a copy-to-clipboard button (if present)
@@ -224,11 +226,12 @@ def get_dynamic_soup_by_xpath(
     driver.get(url)
 
     if consent_xpath:
-        accept_consent(driver, consent_xpath)
+        _accept_consent(driver, consent_xpath)
 
     try:
-        element = WebDriverWait(driver, timeout).until(
-            EC.presence_of_element_located((By.XPATH, xpath)))
+        element = _wait_for_elements(driver, xpath, *halt_xpaths, timeout=timeout)
+        if not element:
+            raise NoSuchElementException(f"Element specified by {xpath!r} is not present")
         _log.info(f"Page has been loaded and element specified by {xpath!r} is present")
         page_source, soup2 = driver.page_source, None
         if click:
@@ -238,7 +241,7 @@ def get_dynamic_soup_by_xpath(
 
         clipboard = None
         if clipboard_xpath:
-            clipboard = click_for_clipboard(driver, clipboard_xpath)
+            clipboard = _click_for_clipboard(driver, clipboard_xpath)
 
         return soup, soup2, clipboard
 
@@ -256,7 +259,7 @@ def throttled_dynamic_soup_by_xpath(
     return get_dynamic_soup_by_xpath(url, xpath, click, consent_xpath, clipboard_xpath, timeout)
 
 
-def accept_consent(driver: WebDriver, xpath: str, timeout=SELENIUM_TIMEOUT / 2) -> None:
+def _accept_consent(driver: WebDriver, xpath: str, timeout=SELENIUM_TIMEOUT / 2) -> None:
     """Accept consent by clicking element located by ``xpath`` with the passed Chrome
     webdriver.
 
@@ -290,7 +293,7 @@ def accept_consent(driver: WebDriver, xpath: str, timeout=SELENIUM_TIMEOUT / 2) 
         raise
 
 
-def click_for_clipboard(driver: WebDriver, xpath: str, timeout=SELENIUM_TIMEOUT / 2) -> str:
+def _click_for_clipboard(driver: WebDriver, xpath: str, timeout=SELENIUM_TIMEOUT / 2) -> str:
     """Click element located by ``xpath`` with the passed Chrome webdriver and return clipboard
     contents.
 
@@ -303,11 +306,10 @@ def click_for_clipboard(driver: WebDriver, xpath: str, timeout=SELENIUM_TIMEOUT 
     Args:
         driver: a Chrome webdriver object
         xpath: XPath to locate the main element
-        timeout: timeout used in attempted actions (consent timeout is halved)
+        timeout: timeout used in attempted actions
 
     Returns:
-        BeautifulSoup object (or two such objects if located element was clicked) from dynamically
-        loaded page source
+        string clipboard content
     """
     _log.info("Attempting to click an element to populate clipboard...")
 
@@ -323,3 +325,37 @@ def click_for_clipboard(driver: WebDriver, xpath: str, timeout=SELENIUM_TIMEOUT 
         driver.quit()
         _log.error(f"Timed out waiting for element specified by {xpath!r} to be present")
         raise
+
+
+def _wait_for_elements(
+        driver: WebDriver, xpath: str, *halt_xpath: str,
+        timeout=SELENIUM_TIMEOUT) -> WebElement | None:
+    """Wait for elements specified by ``xpath`` and ``halt_xpath`` to be present in the current
+    page.
+
+    If ``xpath`` element is located return it. If any element designated by``halt_xpath`` is
+    located return `None`.
+
+    Args:
+        driver: a Chrome webdriver object
+        xpath: XPath to locate the main element
+        halt_xpath: XPaths to locate elements that should halt the wait
+        timeout: timeout used in attempted actions
+    """
+    if halt_xpath:
+        WebDriverWait(driver, timeout).until(
+        EC.any_of(
+            EC.presence_of_element_located((By.XPATH, xpath)),
+            *[EC.presence_of_element_located((By.XPATH, xp)) for xp in halt_xpath]
+        ))
+
+        # check which element was found
+        elements = driver.find_elements(By.XPATH, xpath)
+        if elements:
+            return elements[0]
+
+        _log.warning("Halting element found")
+        return None
+
+    return WebDriverWait(driver, timeout).until(
+        EC.presence_of_element_located((By.XPATH, xpath)))

mtgcards/yt.py
@@ -370,8 +370,12 @@ class Video:
             else:
                 if line == "Deck":
                     deck_lines.append(line)
+                    if not len(deck_lines) > 1:
+                        other_lines.append(line)
                 elif line == "Sideboard":
                     sideboard_lines.append(line)
+                    if not len(sideboard_lines) > 1:
+                        other_lines.append(line)
                 # prevent parsing two decklists as one
                 if not len(deck_lines) > 1 and not len(sideboard_lines) > 1:
                     other_lines.append(line)