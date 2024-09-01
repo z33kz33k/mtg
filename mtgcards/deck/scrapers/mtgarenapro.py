a7616976e9b04a078809c532169cc62cc78d791e
z33k <piotr.mazik@protonmail.com>
2024-09-01 12:01:13 +0200

    Get JSON deck data in MTGArenaPRO scraper (WIP)

--- mtgcards/deck/scrapers/mtgarenapro.py
+++ mtgcards/deck/scrapers/mtgarenapro.py
@@ -8,27 +8,10 @@
 
 """
 import logging
-import time
-from datetime import datetime
-
-import pyperclip
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 from mtgcards import Json
-from mtgcards.deck import Deck, InvalidDeck, Mode, ParsingState
 from mtgcards.deck.scrapers import DeckScraper
-from mtgcards.scryfall import all_formats
-from mtgcards.utils import extract_int, timed
-from mtgcards.utils.scrape import ScrapingError, getsoup, \
-    http_requests_counted, throttled_soup
-from utils.scrape import SELENIUM_TIMEOUT
+from mtgcards.utils.scrape import getsoup
 
 _log = logging.getLogger(__name__)
 
@@ -36,20 +19,13 @@ _log = logging.getLogger(__name__)
 class MtgArenaProScraper(DeckScraper):
     """Scraper of MTGArena.Pro decklist page.
     """
-    # _CONSENT_XPATH = "//span[text()='Accept All']"
-    # _CONSENT_XPATH = "//a[contains(@class, 'cmptxt_btn_yes') and @role='button']"
-    _EXPORT_DECK_XPATH = "//div[text()='Export deck']"
-    _COPY_OUTPUT_XPATH = "//div[text()='Copy output']"
 
     def __init__(self, url: str, metadata: Json | None = None) -> None:
         super().__init__(url, metadata)
         self._soup = getsoup(self.url)
-        # try:
-        #     self._clipboard, self._soup = self._get_clipboard_and_soup()
-        #     self._scrape_metadata()
-        #     self._scrape_deck()
-        # except TimeoutException:
-        #     _log.warning(f"Scraping failed due to Selenium timing out")
+        self._json_data = self._get_json()
+        self._scrape_metadata()
+        self._scrape_deck()
 
     @staticmethod
     def is_deck_url(url: str) -> bool:  # override
@@ -60,30 +36,8 @@ class MtgArenaProScraper(DeckScraper):
         url = DeckScraper.sanitize_url(url)
         return url
 
-    # @timed("getting clipboard and soup")
-    # def _get_clipboard_and_soup(self) -> tuple[str, BeautifulSoup]:
-    #     driver = webdriver.Chrome()
-    #     _log.info(f"Webdriving using Chrome to: '{self.url}'...")
-    #     driver.get(self.url)
-    #
-    #     soup = BeautifulSoup(driver.page_source, "lxml")
-    #     try:
-    #         for click_xpath in (self._EXPORT_DECK_XPATH, self._COPY_OUTPUT_XPATH):
-    #             # element = driver.find_element(By.XPATH, click_xpath)
-    #             element = WebDriverWait(driver, SELENIUM_TIMEOUT).until(
-    #                 EC.presence_of_element_located((By.XPATH, click_xpath)))
-    #             driver.execute_script("arguments[0].click();", element)
-    #
-    #         time.sleep(0.6)
-    #         clipboard = pyperclip.paste()
-    #
-    #         return clipboard, soup
-    #
-    #     except TimeoutException:
-    #         _log.error(f"Timed out waiting for element specified to be present")
-    #         raise
-    #     finally:
-    #         driver.quit()
+    def _get_json(self) -> Json:
+        return self.dissect_js("var precachedDeck=", '"card_ids":', lambda s: s + '"card_ids":[]}')
 
     def _scrape_metadata(self) -> None:  # override
         pass
