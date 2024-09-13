3c3853586f886399d8436a1d1b7822a7f6d6988f
z33k <piotr.mazik@protonmail.com>
2024-09-14 00:12:15 +0200

    Add metadata scraping to TopDecked scraper (#99)

--- mtgcards/deck/scrapers/topdecked.py
+++ mtgcards/deck/scrapers/topdecked.py
@@ -8,35 +8,40 @@
 
 """
 import logging
-import time
-from datetime import datetime
 
-import dateutil.parser
-from bs4 import Tag
 from selenium import webdriver
-from selenium.common import ElementClickInterceptedException
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 from mtgcards import Json
 from mtgcards.deck.scrapers import DeckScraper
-from mtgcards.scryfall import Card
-from mtgcards.utils import extract_int
-from mtgcards.utils.scrape import ScrapingError, get_dynamic_soup_by_xpath, getsoup, \
-    SELENIUM_TIMEOUT, scroll_down, scroll_down_with_arrows, scroll_with_mouse_wheel, click_for_clipboard
+from mtgcards.utils.scrape import SELENIUM_TIMEOUT, click_for_clipboard
+from mtgcards.utils import get_date_from_ago_text
 
 _log = logging.getLogger(__name__)
+
+
+def _sanitize_element_text(text: str) -> str:
+    text = text.strip()
+    if "\n" in text:
+        text, *_ = text.split("\n")
+    return text
 
 
 class TopDeckedScraper(DeckScraper):
     """Scraper of TopDecked decklist page.
     """
+    _CONSENT_XPATH = "//ion-button[contains(., 'Ok!')]"
+    _CONSENT_TIMEOUT = 30
     _SHARE_XPATH = "//ion-button[@tourstep='decklist_view_share']"
     _ARENA_XPATH = "//button[descendant::*[contains(text(), 'Share to Arena')]]"
+    _ARENA_DELAY = 4
+    _NAME_XPATH = "//ion-title[contains(@class, 'header__title')]"
+    _FMT_XPATH = "//span[contains(@class, 'format') and contains(@class, 'text-uppercase')]"
+    _DATE_XPATH = ("//span[contains(@class, 'prefix') and contains(@class, 'text-none') and "
+                   "contains(@class, 'ng-star-inserted')]")
 
     def __init__(self, url: str, metadata: Json | None = None) -> None:
         super().__init__(url, metadata)
@@ -56,12 +61,28 @@ class TopDeckedScraper(DeckScraper):
             _log.info(f"Webdriving using Chrome to: '{self.url}'...")
             driver.get(self.url)
 
-            share = WebDriverWait(driver, SELENIUM_TIMEOUT).until(
-                EC.element_to_be_clickable((By.XPATH, self._SHARE_XPATH)))
-            share.click()
-            _log.info("Share button clicked")
+            # consent
+            consent = WebDriverWait(driver, self._CONSENT_TIMEOUT).until(
+                EC.element_to_be_clickable((By.XPATH, self._CONSENT_XPATH)))
+            consent.click()
+            WebDriverWait(driver, SELENIUM_TIMEOUT / 2).until_not(
+                EC.presence_of_element_located((By.XPATH, self._CONSENT_XPATH)))
+            _log.info("Consent pop-up closed")
 
-            arena = click_for_clipboard(driver, self._ARENA_XPATH, 5)
+            # metadata
+            name_el = driver.find_element(By.XPATH, self._NAME_XPATH)
+            self._metadata["name"] = _sanitize_element_text(name_el.text)
+            fmt_el = driver.find_element(By.XPATH, self._FMT_XPATH)
+            self._update_fmt(_sanitize_element_text(fmt_el.text))
+            date_el = driver.find_element(By.XPATH, self._DATE_XPATH)
+            self._metadata["date"] = get_date_from_ago_text(_sanitize_element_text(date_el.text))
+
+            # arena decklist
+            share_btn = WebDriverWait(driver, SELENIUM_TIMEOUT).until(
+                EC.element_to_be_clickable((By.XPATH, self._SHARE_XPATH)))
+            share_btn.click()
+            _log.info("Share button clicked")
+            arena = click_for_clipboard(driver, self._ARENA_XPATH, self._ARENA_DELAY)
             return arena.splitlines()
 
     def _scrape_metadata(self) -> None:  # override
