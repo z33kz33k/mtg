8c0eaf7c9f66d07fda0564bd07891cbba21dc2c1
z33k <piotr.mazik@protonmail.com>
2026-03-30 02:19:52 +0200

    Use threads to speed up channels' dumping (#462)

--- mtg/data/dump.py
+++ mtg/data/dump.py
@@ -10,17 +10,18 @@
 import itertools
 import logging
 from collections import defaultdict
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from datetime import datetime
 from operator import attrgetter
 from pathlib import Path
 
-from sqlalchemy import select
+from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 from tqdm import tqdm
 
 from mtg.constants import OUTPUT_DIR
 from mtg.data.db import ENGINE
-from mtg.data.models import Channel, Decklist, FailedUrl
+from mtg.data.models import Channel, Decklist, FailedUrl, Snapshot
 from mtg.lib.common import get_timestamp, timed
 from mtg.lib.json import to_json
 
@@ -90,53 +91,68 @@ class Dumper:
         return self._get_channel_dir(channel_yt_id) / self.SNAPSHOT_FILE_TEMPLATE.format(
             channel_yt_id=channel_yt_id, scrape_time=get_timestamp(dt=scrape_time))
 
-    # TODO: probably needs to be sped up with a thread pool
-    def _dump_channels(self) -> None:
-        channels_count, snapshots_count = 0, 0
+    def _process_channel(self, channel_id: int) -> int:
+        snapshots_count = 0
         with Session(ENGINE) as session:
-            stmt = select(Channel)
-            for channel in tqdm(session.scalars(stmt).all(), desc="Dumping channels..."):
+            channel = session.get(Channel, channel_id)
 
-                for snapshot in channel.snapshots:
-                    snapshot_dst = self._get_snapshot_file(
-                        channel_yt_id=channel.yt_id, scrape_time=snapshot.scrape_time)
-                    snapshot_data = {
-                        "id": channel.yt_id,
-                        "title": snapshot.title,
-                        "description": snapshot.description,
-                        "tags": [t.text for t in snapshot.tags],
-                        "subscribers": snapshot.subscribers,
-                        "scrape_time": snapshot.scrape_time,
-                        "videos": []
+            for snapshot in channel.snapshots:
+                snapshot_dst = self._get_snapshot_file(
+                    channel_yt_id=channel.yt_id, scrape_time=snapshot.scrape_time)
+                snapshot_data = {
+                    "id": channel.yt_id,
+                    "title": snapshot.title,
+                    "description": snapshot.description,
+                    "tags": [t.text for t in snapshot.tags],
+                    "subscribers": snapshot.subscribers,
+                    "scrape_time": snapshot.scrape_time,
+                    "videos": []
+                }
+
+                # sort oldest-first
+                for video in sorted(snapshot.videos, key=attrgetter("publish_time")):
+                    video_data = {
+                        "id": video.yt_id,
+                        "title": video.title,
+                        "description": video.description,
+                        "keywords": [kw.text for kw in video.keywords],
+                        "publish_time": video.publish_time,
+                        "views": video.views,
+                        "decks": []
                     }
+                    if video.comment is not None:
+                        video_data["comment"] = video.comment
 
-                    # sort oldest-first
-                    for video in sorted(snapshot.videos, key=attrgetter("publish_time")):
-                        video_data = {
-                            "id": video.yt_id,
-                            "title": video.title,
-                            "description": video.description,
-                            "keywords": [kw.text for kw in video.keywords],
-                            "publish_time": video.publish_time,
-                            "views": video.views,
-                            "decks": []
+                    for deck in video.decks:
+                        deck_data = {
+                            "metadata": deck.json_metadata,
+                            "decklist_hash": deck.decklist.hash,
                         }
-                        if video.comment is not None:
-                            video_data["comment"] = video.comment
+                        video_data["decks"].append(deck_data)
 
-                        for deck in video.decks:
-                            deck_data = {
-                                "metadata": deck.json_metadata,
-                                "decklist_hash": deck.decklist.hash,
-                            }
-                            video_data["decks"].append(deck_data)
+                    snapshot_data["videos"].append(video_data)
 
-                        snapshot_data["videos"].append(video_data)
+                snapshot_dst.write_text(to_json(snapshot_data))
+                snapshots_count += 1
 
-                    snapshot_dst.write_text(to_json(snapshot_data))
-                    snapshots_count += 1
+        return snapshots_count
 
-                channels_count += 1
+    def _dump_channels(self) -> None:
+        snapshots_count = 0
+        # Session is not thread-safe so only channel IDs are pulled from the db (which is
+        # cheap) in the main thread and then channel gets retrieved and processed in its own
+        # session within each thread
+        with Session(ENGINE) as session:
+            channels_count = session.scalar(select(func.count()).select_from(Channel))
+            channel_ids = session.scalars(select(Channel.id)).all()
+
+        with ThreadPoolExecutor(max_workers=12) as executor:
+            futures = [executor.submit(self._process_channel, chid) for chid in channel_ids]
+
+            with tqdm(total=channels_count, desc="Dumping channels...") as pbar:
+                for future in as_completed(futures):
+                    snapshots_count += future.result()
+                    pbar.update(1)
 
         _log.info(f"Dumped {snapshots_count:,} snapshots of {channels_count:,} channels.")
 
